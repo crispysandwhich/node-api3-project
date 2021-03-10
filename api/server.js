d29441dee130f3f79ed1fe154b60a8ce849f92b0
de_gentlem4n <th3g3n3lm4n@gmail.com>
2021-03-10 18:00:23 -0500

    server finnese

--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const morgan = require('morgan')
+const cors = require('cors')
 
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
-
+server.use(express.json())
+server.use(morgan('dev'))
+server.use(cors())
 // global middlewares and the user's router need to be connected here
 
 server.get('/', (req, res) => {
